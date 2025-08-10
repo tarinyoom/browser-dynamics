fc20f08384111867f1a9636d6e61d78287e50144
Adam C Reynolds <adam.chiu.reynolds@gmail.com>
2025-08-10 16:18:25 -0700

    Remove physics update

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
-import { globals } from './constants';
 import { initializeArena, step } from './simulation'; 
 import { createView, drawFrame } from './view';
 import { isDev } from './env';
 
 function makeAnimation(view: View, arena: Arena) {
-  let accumulator = 0;
-
   const animation = () => {
     requestAnimationFrame(animation);
-
-    let frameTime = view.clock.getDelta();
-    accumulator += frameTime;
-
-    let steps = 0;
-    while (accumulator >= globals.timestep && steps < globals.maxTimestepsPerFrame) {
-      step(arena);
-      accumulator -= globals.timestep;
-      steps++;
-    }
-
+    step(arena);
     drawFrame(view, arena.positions, arena.densities);
   }
   return animation;
